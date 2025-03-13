7c00468040115b7d4e79921697fbb9e24766ea5b
“gc” <guocan@enjoypartytime.com>
2025-03-13 12:07:53 +0800

    1、添加清理Glide缓存功能；2、显示图片加载时长

--- app/src/main/java/com/enjoypartytime/testdemo/opengl/image/ImageActivity.java
+++ app/src/main/java/com/enjoypartytime/testdemo/opengl/image/ImageActivity.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.blankj.utilcode.util.ToastUtils;
+import com.bumptech.glide.Glide;
 import com.enjoypartytime.testdemo.R;
 import com.enjoypartytime.testdemo.opengl.image.adapter.ImageAdapter;
 
@@ -24,6 +26,8 @@ public class ImageActivity extends Activity {
 
     private String type = "webp";
     private boolean isLocal = true;
+    private long loadTime = 0;
+    private boolean countTime = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +39,7 @@ public class ImageActivity extends Activity {
 
         TextView tvType = findViewById(R.id.tv_image_type);
         TextView tvLocal = findViewById(R.id.tv_image_local);
+        TextView tvClearCache = findViewById(R.id.tv_image_clear_cache);
         TextView tvOrigin = findViewById(R.id.tv_image_origin);
         TextView tvFilter = findViewById(R.id.tv_image_filter);
 
@@ -55,16 +60,39 @@ public class ImageActivity extends Activity {
             tvLocal.setText(isLocal ? "本地路径" : "网络图片");
         });
 
+        tvClearCache.setOnClickListener(v -> {
+            Glide.get(getApplicationContext()).clearMemory();
+            new Thread(() -> Glide.get(getApplicationContext()).clearDiskCache()).start();
+            ToastUtils.showShort("缓存清理完成");
+        });
+
         swipeRefreshLayout.setOnRefreshListener(() -> swipeRefreshLayout.setRefreshing(false));
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        recyclerView.getViewTreeObserver()
+                .addOnGlobalLayoutListener(() -> {
+                    if (countTime) {
+                        countTime = false;
+                        loadTime = System.currentTimeMillis() - loadTime;
+                        ToastUtils.showShort("加载时长=" + loadTime + "ms");
+                    }
+                });
 
         List<String> stringList = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             stringList.add("第" + i + "个选项");
         }
 
-        tvOrigin.setOnClickListener(v -> recyclerView.setAdapter(new ImageAdapter(getApplicationContext(), stringList, type, isLocal, true)));
+        tvOrigin.setOnClickListener(v -> {
+            countTime = true;
+            loadTime = System.currentTimeMillis();
+            recyclerView.setAdapter(new ImageAdapter(getApplicationContext(), stringList, type, isLocal, true));
+        });
 
-        tvFilter.setOnClickListener(v -> recyclerView.setAdapter(new ImageAdapter(getApplicationContext(), stringList, type, isLocal, false)));
+        tvFilter.setOnClickListener(v -> {
+            countTime = true;
+            loadTime = System.currentTimeMillis();
+            recyclerView.setAdapter(new ImageAdapter(getApplicationContext(), stringList, type, isLocal, false));
+        });
     }
 }
