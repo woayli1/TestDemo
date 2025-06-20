876772578fa94422d36d37c20a1d2984a4e94ee6
“gc” <guocan@enjoypartytime.com>
2025-06-20 16:54:56 +0800

    修复Live2D页面层级

--- app/src/main/java/com/enjoypartytime/testdemo/opengl/live2d/Live2DActivity.java
+++ app/src/main/java/com/enjoypartytime/testdemo/opengl/live2d/Live2DActivity.java
@@ -1,13 +1,15 @@
 package com.enjoypartytime.testdemo.opengl.live2d;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MotionEvent;
 
 import androidx.annotation.Nullable;
 
-import com.blankj.utilcode.util.ActivityUtils;
 import com.enjoypartytime.testdemo.R;
+import com.enjoypartytime.testdemo.opengl.live2d.full.GLRenderer;
+import com.enjoypartytime.testdemo.opengl.live2d.full.LAppDelegate;
 
 /**
  * author gc
@@ -16,20 +18,77 @@ import com.enjoypartytime.testdemo.R;
  */
 public class Live2DActivity extends Activity {
 
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_2d);
 
-        TextView tvLive2dBasic = findViewById(R.id.tv_live_2d_basic);
-        TextView tvLive2dClickChange = findViewById(R.id.tv_live_2d_click_change);
-        TextView tvLive2dClickResponse = findViewById(R.id.tv_live_2d_click_response);
-        TextView tvLive2dMoveResponse = findViewById(R.id.tv_live_2d_move_response);
-        TextView tvLive2dMore = findViewById(R.id.tv_live_2d_more);
+        glSurfaceView = findViewById(R.id.gl_surface_view_live_2d);
+        glSurfaceView.setEGLContextClientVersion(2);
 
-        tvLive2dBasic.setOnClickListener(view -> ActivityUtils.startActivity(Live2DActivity.this, Live2DBasicActivity.class));
+        GLRenderer glRenderer = new GLRenderer();
+
+        glSurfaceView.setRenderer(glRenderer);
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        LAppDelegate.getInstance().onStart(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        glSurfaceView.onPause();
+        LAppDelegate.getInstance().onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LAppDelegate.getInstance().onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        LAppDelegate.getInstance().onDestroy();
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                LAppDelegate.getInstance().onTouchBegan(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_UP:
+                LAppDelegate.getInstance().onTouchEnd(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                LAppDelegate.getInstance().onTouchMoved(pointX, pointY);
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
 }
